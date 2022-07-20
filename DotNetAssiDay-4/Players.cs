3e9c74a20f415239d3659eec5a2d13481eee1a8f
srevanwar09 <srevanwar09@domain.com>
2022-07-20 23:39:09 +0530

    SnakeLadderSimulator project added

---
+++ DotNetAssiDay-4/Players.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnakeLadderSim
+{
+    internal class Players
+    {
+        const int LADDER = 1;
+        const int SNAKE = 2;
+        const int WINNING_POINT = 100;
+        public int gamePlay(int player)
+        {
+            int tempPosition, diceCount = 0, position = 0;
+            while (position < WINNING_POINT)
+            {
+                Random random = new Random();
+                int option = random.Next(0, 3);
+                int rollDie = random.Next(1, 7);
+                int againRollDie = random.Next(1, 7);
+                diceCount++;
+                switch (option)
+                {
+                    case LADDER:
+                        tempPosition = position + rollDie;
+                        if (tempPosition > 100)
+                        {
+                            position += 0;
+                        }
+                        else
+                        {
+                            tempPosition += againRollDie;
+                            diceCount++;
+                            if (tempPosition > 100)
+                            {
+                                position += 0;
+                            }
+                            else
+                            {
+                                position = tempPosition;
+                            }
+                        }
+                        break;
+                    case SNAKE:
+                        tempPosition = position - rollDie;
+                        if (tempPosition <= 0)
+                        {
+                            position = 0;
+                        }
+                        else
+                        {
+                            position = tempPosition;
+                        }
+                        break;
+                    default:
+                        position += 0;
+                        break;
+                }
+                Console.WriteLine("Player " + player + " position is " + position);
+            }
+            Console.WriteLine("The " + diceCount + " times the dice was played to win the game.");
+            return diceCount;
+        }
+    }
+}
---
+++ DotNetAssiDay-4/Program.cs
@@ -0,0 +1,17 @@
+﻿using SnakeLadderSim;
+Players play = new Players();
+Console.WriteLine("Welcome in Snake & Ladder Simulator.");
+int player1 = play.gamePlay(1);
+int player2 = play.gamePlay(2);
+if (player1 < player2)
+{
+    Console.WriteLine("Player 1 is a Winner.");
+}
+else if (player1 > player2)
+{
+    Console.WriteLine("Player 2 is a Winner.");
+}
+else
+{
+    Console.WriteLine("Its a tie between Player 1 and 2.");
+}

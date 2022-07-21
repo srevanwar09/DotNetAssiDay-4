e10e849cd8d20a29cc5a81ab9d5b45e32d9348f3
srevanwar09 <srevanwar09@domain.com>
2022-07-21 21:44:45 +0530

    Day-6 program added

--- DotNetAssiDay-4/Players.cs
+++ DotNetAssiDay-4/Players.cs
@@ -1,42 +1,56 @@
-﻿const int LADDER = 1;
-const int SNAKE = 2;
-const int WINNING_POINT = 100;
-Console.WriteLine("Welcome in Snake & Ladder Simulator.");
-int tempPosition;
-for (int position = 0; position <= WINNING_POINT; position++)
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnakeLadderSimulator
 {
-    Random random = new Random();
-    int option = random.Next(0, 3);
-    int rolldie = random.Next(1, 7);
-    switch (option)
+    internal class Player
     {
-        case LADDER:
-            tempPosition = position + rolldie;
-            if (tempPosition > 100)
+        const int LADDER = 1;
+        const int SNAKE = 2;
+        const int WINNING_POINT = 100;
+        public void gamePlay()
+        {
+            int tempPosition, diceCount = 0, position = 0;
+            while (position < WINNING_POINT)
             {
-                position += 0;
+                Random random = new Random();
+                int option = random.Next(0, 3);
+                int rolldie = random.Next(1, 7);
+                diceCount++;
+                switch (option)
+                {
+                    case LADDER:
+                        tempPosition = position + rolldie;
+                        if (tempPosition > 100)
+                        {
+                            position += 0;
+                        }
+                        else
+                        {
+                            position = tempPosition;
+                        }
+                        break;
+                    case SNAKE:
+                        tempPosition = position - rolldie;
+                        if (tempPosition <= 0)
+                        {
+                            position = 0;
+                        }
+                        else
+                        {
+                            position = tempPosition;
+                        }
+                        break;
+                    default:
+                        position += 0;
+                        break;
+                }
+                Console.WriteLine("Player 1 position is " + position);
             }
-            else
-            {
-                position = tempPosition;
-            }
-            break;
-        case SNAKE:
-            tempPosition = position - rolldie;
-            if (tempPosition <= 0)
-            {
-                position = 0;
-            }
-            else
-            {
-                position = tempPosition;
-            }
-            break;
-        default:
-            position += 0;
-            break;
+            Console.WriteLine("The " + diceCount + " times the dice was played to win the game.");
+        }
     }
-    Console.WriteLine("Player 1 position is " + position);
 }
-
-
--- DotNetAssiDay-4/Program.cs
+++ DotNetAssiDay-4/Program.cs
@@ -1,14 +1,5 @@
-﻿class Programs
-{
-    static void main(String[] Args)
-    {
-        int position = 0;
-        Console.WriteLine("welcome in Snake & Ladder Simulator.");
-        Random random = new Random();
-        int rolldie = random.Next(1, 7);
-        Console.WriteLine("Player 1 position is " + position);
-        Console.WriteLine("Roll Die value: " + rolldie);
+﻿using SnakeLadderSimulator;
 
-
-    }
-}
+Player play = new Player();
+Console.WriteLine("Welcome in Snake & Ladder Simulator.");
+play.gamePlay();
